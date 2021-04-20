88f28cc2935b413570ce8d480f4623f511e30085
Andru Estes <andru.estes@gmail.com>
2021-04-19 22:51:09 -0500

    Lesson 17 - need to restart it

--- catch-of-the-day/src/components/AddFishForm.js
+++ catch-of-the-day/src/components/AddFishForm.js
@@ -1,15 +1,51 @@
 import React from "react";
 
 class AddFishForm extends React.Component {
+  nameRef = React.createRef();
+  priceRef = React.createRef();
+  statusRef = React.createRef();
+  descRef = React.createRef();
+  imageRef = React.createRef();
+
+  createFish = (event) => {
+    // Stop form from submitting
+    event.preventDefault();
+    const fish = {
+      name: this.nameRef.current.value,
+      price: parseFloat(this.priceRef.current.value),
+      status: this.statusRef.current.value,
+      desc: this.descRef.current.value,
+      image: this.imageRef.current.value,
+    };
+    this.props.addFish(fish);
+    // Refresh the form
+    event.currentTarget.reset();
+  };
+
   render() {
     return (
-        <form>
-            <input name="name" type="text" placeholder="name" />
-            <input name="price" type="text" placeholder="price"/>
-            <input name="status" type="text" placeholder="status"/>
-            <input name="desc" type="text" placeholder="desc"/>
-            <input name="image" type="text" placeholder="image"/>
-        </form>
+      <form className="fish-edit" onSubmit={this.createFish}>
+        <input name="name" ref={this.nameRef} type="text" placeholder="name" />
+        <input
+          name="price"
+          ref={this.priceRef}
+          type="text"
+          placeholder="price"
+        />
+        <select name="status" ref={this.statusRef}>
+          <option value="available">In Stock</option>
+          <option value="unavailable">Sold Out</option>
+        </select>
+        <input name="desc" ref={this.descRef} type="text" placeholder="desc" />
+        <input
+          name="image"
+          ref={this.imageRef}
+          type="text"
+          placeholder="image"
+        />
+        <textarea name="desc" type="text" placeholder="Desc" />
+        <button type="submit">+ Add Fish</button>
+      </form>
     );
   }
 }
--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -1,20 +1,64 @@
-import React from "react"
-import Header from './Header';
-import Order from './Order';
-import Inventory from './Inventory';
+import React from "react";
+import Header from "./Header";
+import Order from "./Order";
+import Inventory from "./Inventory";
+import fishes from "../sample-fishes";
+import Fish from "./Fish";
 
 class App extends React.Component {
-    render(){
-        return(
-            <div className="catch-of-the-day">
-                <div className="menu">
-                    <Header tagline="Fresh Seafood Daily" />
-                </div>
-                <Order />
-                <Inventory />
-            </div>
-        )
-    }
+  state = {
+    fishes: {},
+    order: {},
+  };
+
+  addFish = (fish) => {
+    // Take a copy of existing state
+    const fishes = { ...this.state.fishes };
+    // Add new fish to fishes
+    fishes[`fish${Date.now()}`] = fish;
+    // Set new fishes object to state
+    this.setState({
+      fishes: fishes,
+    });
+  };
+
+  loadSampleFishes = () => {
+    this.setState({ fishes: fishes });
+  };
+
+  addToOrder = (key) => {
+    // Take copy of state
+    const order = { ...this.state.order };
+    // Add to order or update total of each fish
+    order[key] = order[key] + 1 || 1;
+    // Call setState to update state object
+    this.setState({ order });
+  };
+
+  render() {
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header tagline="Fresh Seafood Daily" />
+          <ul className="fishes">
+            {Object.keys(this.state.fishes).map((key) => (
+              <Fish
+                key={key}
+                index={key}
+                details={this.state.fishes[key]}
+                addToOrder={this.addToOrder}
+              />
+            ))}
+          </ul>
+        </div>
+        <Order fishes={this.state.fishes} order={this.state.order} />
+        <Inventory
+          addFish={this.addFish}
+          loadSampleFishes={this.loadSampleFishes}
+        />
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
